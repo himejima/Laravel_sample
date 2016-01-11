9c3efcd1aee6c8efae6bf06e928bedb48784a963
himejimasandesu <aganoyot@gmail.com>
2016-01-11 18:58:59 +0900

    first commit

---
+++ blog/app/Http/Controllers/TaskController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller
+{
+    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        $tasks = Task::where('user_id', $request->user()->id)->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+    }
+
+    /**
+     * Destroy the given task.
+     *
+     * @param  Request  $request
+     * @param  Task  $task
+     * @return Response
+     */
+    public function destroy(Request $request, Task $task)
+    {
+        //
+        $this->authorize('destroy', $task);
+        $task->delete();
+
+        return redirect('/tasks');
+    }
+}
